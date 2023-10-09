075c2bf6123af7f44ee2c2be00d8382d485c78f1
FabioB8a <fabiobuitra@gmail.com>
2023-10-09 12:47:50 -0500

    Matriz cambios

--- matriz.js
+++ matriz.js
@@ -1,6 +1,6 @@
 // matrix.js
 
-// Function to get query parameters
+// Obtener filas y columna
 function getQueryParams() {
     const queryParams = new URLSearchParams(window.location.search);
     const rows = parseInt(queryParams.get("rows"));
@@ -10,30 +10,78 @@ function getQueryParams() {
     return { rows, columns };
 }
 
-function displayMatrix(matrix) {
+// Function to generate a Kakuro board
+function generateKakuroBoard(rows, columns) {
+    const board = [];
+    for (let i = 0; i < rows; i++) {
+        const row = [];
+        for (let j = 0; j < columns; j++) {
+            if (i % 2 === 0 && j % 2 === 0) {
+                // Fill even-indexed rows and columns with input fields
+                const input = document.createElement("input");
+                input.type = "number";
+                input.min = 1;
+                input.max = 9;
+                input.className = "kakuro-cell";
+                row.push(input);
+            } else {
+                // Fill other cells as empty spaces
+                const kakuroCell = document.createElement("div");
+                kakuroCell.className = "kakuro-cell";
+                
+                // Create the diagonal numbers
+                const topNumber = document.createElement("div");
+                topNumber.className = "kakuro-top-number";
+                topNumber.contentEditable = true; // Allow user to edit the number
+                topNumber.textContent = "1"; // Initial value (you can set it as you like)
+                
+                const bottomNumber = document.createElement("div");
+                bottomNumber.className = "kakuro-bottom-number";
+                bottomNumber.contentEditable = true; // Allow user to edit the number
+                bottomNumber.textContent = "2"; // Initial value (you can set it as you like)
+                
+                // Append numbers to the Kakuro cell
+                kakuroCell.appendChild(topNumber);
+                kakuroCell.appendChild(bottomNumber);
+                
+                row.push(kakuroCell);
+            }
+        }
+        board.push(row);
+    }
+    return board;
+}
+
+// Imprimir la matriz (Kakuro board)
+function displayKakuroBoard(board) {
     const table = document.getElementById("matrix-table");
-    for (let i = 0; i < matrix.length; i++) {
+    for (let i = 0; i < board.length; i++) {
         const row = document.createElement("tr");
-        for (let j = 0; j < matrix[i].length; j++) {
+        for (let j = 0; j < board[i].length; j++) {
             const cell = document.createElement("td");
-            cell.textContent = matrix[i][j];
+            const boardElement = board[i][j];
+            
+            if (boardElement instanceof HTMLInputElement) {
+                // Si es un campo de entrada (input), agregalo a la celda
+                cell.appendChild(boardElement);
+            } else if (boardElement instanceof HTMLDivElement) {
+                // Si es una celda Kakuro (div), agregalo a la celda
+                cell.appendChild(boardElement);
+            } else {
+                // Si no es ninguno de los anteriores, establece el contenido de texto de la celda
+                cell.textContent = boardElement;
+            }
             row.appendChild(cell);
         }
         table.appendChild(row);
     }
 }
 
-// Get rows and columns from the URL
+
 const { rows, columns } = getQueryParams();
-// Create the matrix with the specified rows and columns
-const matrix = [];
-for (let i = 0; i < rows; i++) {
-    matrix.push([]);
-    for (let j = 0; j < columns; j++) {
-        // Initialize matrix values as needed
-        // In this example, it's initialized with zeros.
-        matrix[i][j] = 0;
-    }
-}
-console.log(matrix);
-displayMatrix(matrix);
+const kakuroBoard = generateKakuroBoard(rows, columns);
+
+// Impresión y display del tablero Kakuro
+console.log(kakuroBoard);
+displayKakuroBoard(kakuroBoard);
+
